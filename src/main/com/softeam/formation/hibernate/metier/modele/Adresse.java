f71893ea936c9dcf9a35e5309eacc3a3d1e40832
Ludwig En Formation (rennes) <ludwig.en.formation+github@gmail.com>
2018-01-23 15:31:39 +0100

    Exo

--- src/main/com/softeam/formation/hibernate/metier/modele/Adresse.java
+++ src/main/com/softeam/formation/hibernate/metier/modele/Adresse.java
@@ -7,6 +7,7 @@ import javax.persistence.Id;
 import javax.persistence.*;
 
 @Entity
+@Table(name = "T_ADRESSE")
 public class Adresse
 {
 	@Id
--- src/test/com/softeam/formation/jpa/test/Exercice02.java
+++ src/test/com/softeam/formation/jpa/test/Exercice02.java
@@ -6,7 +6,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import javax.persistence.Query;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,6 +22,9 @@ public class Exercice02
 		adresses.add(new Adresse("a", "b", "c", "56000", "Morbihan Indépendant"));
 		adresses.add(new Adresse("d", "e", "f", "29000", "Humideland"));
 		adresses.add(new Adresse("a", "h", "i", "22000", "Pluiville"));
+		adresses.add(new Adresse("j", null, "k", "22000", "Pluiville"));
+		adresses.add(new Adresse("k", "m", null, "22000", "Pluiville"));
+		adresses.add(new Adresse("n", "o", "p", null, "Pluiville"));
 
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hibernate");
 		EntityManager em = emf.createEntityManager();
