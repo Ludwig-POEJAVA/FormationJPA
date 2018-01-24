36f3fa6879144430976dd29ded7ac7d999c8f747
Ludwig En Formation (rennes) <ludwig.en.formation+github@gmail.com>
2018-01-24 12:27:21 +0100

    exo4

--- src/main/com/softeam/formation/hibernate/metier/modele/Identite.java
+++ src/main/com/softeam/formation/hibernate/metier/modele/Identite.java
@@ -1,6 +1,72 @@
 package com.softeam.formation.hibernate.metier.modele;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Embeddable
 public class Identite
 {
+	@Id
+	@Column(name = "IDENTITE_ID")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long identite_id;
+
+	private String nom;
+	private String prenom;
+	private String initiales;
+
+	public Identite()
+	{
+	}
+
+	public Identite(String nom, String prenom, String initiales)
+	{
+		this.nom = nom;
+		this.prenom = prenom;
+		this.initiales = initiales;
+	}
+
+	public long getIdentite_id()
+	{
+		return identite_id;
+	}
+
+	public void setIdentite_id(long identite_id)
+	{
+		this.identite_id = identite_id;
+	}
+
+	public String getNom()
+	{
+		return nom;
+	}
+
+	public void setNom(String nom)
+	{
+		this.nom = nom;
+	}
+
+	public String getPrenom()
+	{
+		return prenom;
+	}
+
+	public void setPrenom(String prenom)
+	{
+		this.prenom = prenom;
+	}
+
+	public String getInitiales()
+	{
+		return initiales;
+	}
+
+	public void setInitiales(String initiales)
+	{
+		this.initiales = initiales;
+	}
 
 }
--- src/main/com/softeam/formation/hibernate/metier/modele/Personne.java
+++ src/main/com/softeam/formation/hibernate/metier/modele/Personne.java
@@ -1,6 +1,70 @@
 package com.softeam.formation.hibernate.metier.modele;
 
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Personne
 {
+	@Id
+	@Column(name = "PERSONNE_ID")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long personne_id;
+	private String email;
+	private int age;
+	@Embedded
+	private Identite identite;
+
+	public Personne()
+	{
+	}
+
+	public Personne(String name, int age)
+	{
+		this.email = name;
+		this.age = age;
+	}
+
+	public long getPersonne_id()
+	{
+		return personne_id;
+	}
+
+	public void setPersonne_id(long personne_id)
+	{
+		this.personne_id = personne_id;
+	}
+
+	public String getEmail()
+	{
+		return email;
+	}
+
+	public void setEmail(String email)
+	{
+		this.email = email;
+	}
+
+	public int getAge()
+	{
+		return age;
+	}
+
+	public void setAge(int age)
+	{
+		this.age = age;
+	}
+
+	public Identite getIdentite()
+	{
+		return identite;
+	}
+
+	public void setIdentite(Identite identite)
+	{
+		this.identite = identite;
+	}
 
 }
